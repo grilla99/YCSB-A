5f6691f07cd304f0933b646e4870b68a3b9df8c9
grilla001 <a.grill@student.reading.ac.uk>
2021-04-14 14:54:49 +0100

    Directory restructure

---
+++ src/ycsb_script.sh
@@ -0,0 +1,8 @@
+FILE=/ycsb.config
+# Re write this so that it checks if there's a YCSB directory and the executable file within.
+
+if [ -f "$FILE" ]; then
+	echo  "Client X has YCSB installed"
+else 
+	echo "Client X does not have YCSB installed. Removing node..."
+fi
