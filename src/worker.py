8a6a0bf474bd11d50c7da619c3e090d59d8741e4
grilla001 <a.grill@student.reading.ac.uk>
2021-04-28 19:58:11 +0100

    Working connection strings

--- src/worker.py
+++ src/worker.py
@@ -188,7 +188,7 @@ class Slave:
             insert_start_string = "insertstart=" + str(insert_start)
             insert_count = "insertcount=" + data[7]
             record_count = data[9]
-            connection_string = "mongodb.url=mongodb://172.31.26.152:27017,172.31.18.215:27017,172.31.27.18:27017/?replicaSet=rs0"
+            connection_string = "mongodb.url='mongodb://172.31.26.152:27017,172.31.18.215:27017,172.31.27.18:27017/?replicaSet=rs0'"
 
             # If a file exists already, will write to it. If not it shall create a file and write the output
             # Of the YCSB log to it
@@ -216,7 +216,7 @@ class Slave:
                 run_param = data[2]
                 additional_param = data[3]
                 workload_data = data[4]
-                connection_string = ' mongodb.url="mongodb://172.31.26.152:27017,172.31.18.215:27017,172.31.27.18:27017/?replicaSet=rs0'
+                connection_string = 'mongodb.url="mongodb://172.31.26.152:27017,172.31.18.215:27017,172.31.27.18:27017/?replicaSet=rs0'
                 operation_count = data[7]
                 node = data[8]
 
